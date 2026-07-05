a865f6f293f227a01c17d505c18b540f7139ed96
hyungzn-ai <91618or@gmail.com>
2026-07-05 19:48:33 +0900

    feat: 앱 시작 시 알림 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -42,38 +42,3 @@ class WriteMon extends StatelessWidget {
     );
   }
 }
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-import 'providers/app_provider.dart';
-import 'screens/splash_screen.dart';
-import 'utils/theme.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(const WriteMon());
-}
-
-class WriteMon extends StatelessWidget {
-  const WriteMon({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => AppProvider()..init(),
-      child: Consumer<AppProvider>(
-        builder: (context, provider, _) {
-          return MaterialApp(
-            title: '영작몬',
-            debugShowCheckedModeBanner: false,
-            theme: provider.isLoaded
-                ? provider.currentTheme
-                : AppTheme.buildTheme(AppTheme.purpleColors),
-            home: const SplashScreen(),
-          );
-        },
-      ),
-    );
-  }
-}
